5b34c6153258702f032e763a04c0b59e72727401
Midas Petrych <midaspetrych2@hotmail.com>
2024-12-19 11:02:33 +0100

    Adde build and changed static path to be correct

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 
 <main class="h-full mb-24">
     <div class="text-center text-4xl mt-8 mb-8">Website under construction check back soon!</div>
-    <img class=" justify-self-center" src='src/static/img/ConstructionMinions.webp' alt="undead minion construction"/>
+    <img class=" justify-self-center" src='/img/ConstructionMinions.webp' alt="undead minion construction"/>
 
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+	// Use Vite's preprocessing options
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// Configure the adapter-static for static site deployment
+		adapter: adapter({
+			// Directory to output the build
+			pages: 'build',
+			assets: 'build',
+			// For single-page apps, use 'index.html' as the fallback
+			fallback: null, // Change to 'index.html' if needed
+		}),
+		paths: {
+			// Base path, update this if deploying to a subdirectory
+			base: '',
+		}
 	}
 };
 
